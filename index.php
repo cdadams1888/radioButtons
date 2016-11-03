5beb229a1341b0bec7c161f3cc9a76cc413de174
Christopher Adams <cdadams1888@yahoo.com>
2016-11-02 23:37:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="process.php" method="post">
+    <p>
+        <input type="radio" name="language" value="PHP" />PHP<br />
+        <input type="radio" name="language" value="Python" />Python<br />
+        <input type="radio" name="language" value="JavaScript" />JavaScript<br />
+    </p>
+    <p><input type="submit" value="Submit" /></p>
+</form>
+
---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+
+$language = $_POST['language'];
+
+if(isset($_POST['language'])) {
+    echo $language;
+}
+
+?>
